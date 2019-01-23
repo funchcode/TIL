8860f9804391703199bcb9767943a35417eb36b7
정채환 <funchcode@gmail.com>
2019-01-23 23:02:04 +0900

    VS code setting

---
+++ algorithm/SelectionSort.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// Selection Sort 선택정렬
+// 앞에서부터 순차적으로 최솟값을 찾아낸다.
+class Main {
+    public static void main(String[] args) throws Exception {
+        java.util.Scanner scanner = new Scanner(System.in);
+        String data = scanner.nextLine();
+
+        System.out.println(">>"+data);
+
+        scanner.close();
+    }
+}
