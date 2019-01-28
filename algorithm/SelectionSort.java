edf5e9397048f44dfe14069ea5b0931bbb94a698
정채환 <funchcode@gmail.com>
2019-01-28 23:52:07 +0900

    SelectionSort in JAVA

--- algorithm/SelectionSort.java
+++ algorithm/SelectionSort.java
@@ -1,24 +1,21 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
-// Selection Sort 선택정렬
-// 앞에서부터 순차적으로 최댓값을 찾아낸다.
-class Main {
-    public static void main(String[] args) throws Exception {
+public class SelectionSort {
+    public static void main(String[] args) {
         // Input Data
         int[] data = {9,2,3,1,7,4};
-        for(int i = data.length-1 ; i >= 1 ; i--) {
-            for(int j = 0 ; j < i ; j++) {
-                int max = 0;
-                if(data[j]>data[j+1]) {
-                    max = data[j];
-                    data[j] = data[j+1];
-                    data[j+1] = max;
-                }else{
-                    max = data[j+1];
+
+        for(int i = 0 ; i < data.length-1 ; i++) {
+            int indexMin = i;
+            for(int j = i+1 ; j < data.length ; j++) {
+                if(data[indexMin] > data[j]) {
+                    indexMin = j;
                 }
             }
+            int temp = data[i];
+            data[i] = data[indexMin];
+            data[indexMin] = temp;
         }
-        System.out.print(Arrays.toString(data));
+        System.out.println(Arrays.toString(data));
     }
 }
